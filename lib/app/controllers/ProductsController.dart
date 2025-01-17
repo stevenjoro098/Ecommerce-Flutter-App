3db1d4793c3ddd2911305b31dd3d4e86281a54d9
steve <stevenjoro098@gmail.com>
2025-01-17 10:10:00 +0300

    change URL setup

--- lib/app/controllers/ProductsController.dart
+++ lib/app/controllers/ProductsController.dart
@@ -5,14 +5,15 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import '../models/ProductModel.dart';
+import '../utils/api_constants.dart';
 
 class ProductController extends GetxController{
     var products = [].obs;
     var isLoading = true.obs;
-
+    String categoryProductsUrl = APIConstants.category;
     void fetchProducts(String categoryName) async{
       try{
-        final response = await http.get(Uri.parse('http://127.0.0.1:9000/api/category/products/${categoryName}'));
+        final response = await http.get(Uri.parse('${categoryProductsUrl}${categoryName}'));
         if(response.statusCode==200){
           Future.delayed(Duration(seconds: 1), (){
             final List<dynamic> data = jsonDecode(response.body);
